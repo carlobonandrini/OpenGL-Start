eba26165f62d4b5fb51b00c35a095a9213b7e51d
Carlo Bonandrini <carlo.bonandrini@icloud.com>
2019-08-30 14:39:19 +0200

    Changed linking of GLFW and GLEW to static

---
+++ cmake-build-debug/CMakeFiles/OpenGLStart.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLStart.dir/src/main.cpp.o"
+  "bin/OpenGLStart.pdb"
+  "bin/OpenGLStart"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLStart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
